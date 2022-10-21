2ee0990a6579ba7eb33b3372f8b86215bc25057d
Srinivas Manikanta <97425800+S-1729@users.noreply.github.com>
2022-10-21 21:17:03 +0530

    import java.util.*; class Solution{     public static int isminimum(int [] arr,int k)     {         for(int j=0;j<arr.length;j++)         {             if(arr[j]%k!=0)             return 0;         }         return 1;     }     public static void main(String args[])     {         Scanner scan=new Scanner(System.in);         int n=scan.nextInt();         int l=1;         for(int i=0;i<n;i++)         {             for(int j=i;j<n-1;j++)             {                 System.out.print(" ");             }             for(int k=0;k<l;k++)             {                 System.out.print(i+1);             }             l+=2;             System.out.println();         }     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,15 +1,31 @@
-import java.util.Scanner;
-class Main {
-	public static void main(String[] args) {
-		Scanner scan=new Scanner(System.in);
-		int n=scan.nextInt();
-		for(int i=1;i<=n;i++)
-		{
-		    for(int  j=1;j<=i;j++)
-		    {
-		        System.out.print(j);
-		    }
-		    System.out.println();
-		}
-	}
+import java.util.*;
+class Solution{
+    public static int isminimum(int [] arr,int k)
+    {
+        for(int j=0;j<arr.length;j++)
+        {
+            if(arr[j]%k!=0)
+            return 0;
+        }
+        return 1;
+    }
+    public static void main(String args[])
+    {
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        int l=1;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i;j<n-1;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int k=0;k<l;k++)
+            {
+                System.out.print(i+1);
+            }
+            l+=2;
+            System.out.println();
+        }
+    }
 }
