da412514bb60e23a34b7807878a2a77deb417483
Srinivas Manikanta <97425800+S-1729@users.noreply.github.com>
2022-10-21 21:34:50 +0530

    import java.util.*; class Solution{     public static void main(String args[])     {         Scanner scan=new Scanner(System.in);         int n=scan.nextInt();         for(int i=0;i<n;i++)         {             for(int j=0;j<n;j++)             {                 if(i==j || i+j==n-1)                     System.out.print(n-i+" ");                 else                     System.out.print(" ");             }             System.out.println();         }     } }

--- Print_the_following_pattern.java
+++ Print_the_following_pattern.java
@@ -1,17 +1,19 @@
-import java.util.Scanner;
-class Main {
-	public static void main(String[] args) {
-		Scanner scan=new Scanner(System.in);
-		int n=scan.nextInt();
-		for(int i=1;i<=n;i++)
-		{
-		    for(int  j=i;j<=n;j++)
-		    {
-		       
-		        System.out.print(j+" ");
-		  
-		    }
-		    System.out.println();
-		}
-	}
+import java.util.*;
+class Solution{
+    public static void main(String args[])
+    {
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i==j || i+j==n-1)
+                    System.out.print(n-i+" ");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
 }
