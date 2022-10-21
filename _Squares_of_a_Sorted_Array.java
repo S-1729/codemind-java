129f251876453fdee6166d17f8c99627ae33737f
Srinivas Manikanta <97425800+S-1729@users.noreply.github.com>
2022-10-21 20:01:38 +0530

    import java.util.*; class Solution{     public static void main(String args[])     {         Scanner scan=new Scanner(System.in);         int n=scan.nextInt();         int arr[]=new int[n];         for(int i=0;i<n;i++)         {             arr[i]=scan.nextInt();             arr[i]=arr[i]*arr[i];         }         for(int i=0;i<n;i++)         {             for(int j=i+1;j<n;j++)             {                 if(arr[i]>arr[j])                 {                     int temp=arr[i];                     arr[i]=arr[j];                     arr[j]=temp;                 }             }         }         for(int i=0;i<n;i++)         {             System.out.print(arr[i]+" ");         }     } }

---
+++ _Squares_of_a_Sorted_Array.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Solution{
+    public static void main(String args[])
+    {
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=scan.nextInt();
+            arr[i]=arr[i]*arr[i];
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(arr[i]>arr[j])
+                {
+                    int temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
