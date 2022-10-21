7642dfc62b710cb580fd92836cf39495dfbf6f4b
Srinivas Manikanta <97425800+S-1729@users.noreply.github.com>
2022-10-21 21:28:56 +0530

    import java.util.*; class Solution{     public static void main(String args[])     {         Scanner scan=new Scanner(System.in);         int n=scan.nextInt();         for(int i=0;i<n;i++)         {             for(int j=0;j<n-i;j++)             {                 System.out.print((char)(64+n-i)+" ");             }             System.out.println();         }     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,31 +1,14 @@
 import java.util.*;
 class Solution{
-    public static int isminimum(int [] arr,int k)
-    {
-        for(int j=0;j<arr.length;j++)
-        {
-            if(arr[j]%k!=0)
-            return 0;
-        }
-        return 1;
-    }
     public static void main(String args[])
     {
         Scanner scan=new Scanner(System.in);
         int n=scan.nextInt();
         for(int i=0;i<n;i++)
         {
-            for(int j=i;j<n-1;j++)
+            for(int j=0;j<n-i;j++)
             {
-                System.out.print(" ");
-            }
-            for(int k=i+1;k>1;k--)
-            {
-                System.out.print(k-1);
-            }
-            for(int k=0;k<i+1;k++)
-            {
-                System.out.print(k);
+                System.out.print((char)(64+n-i)+" ");
             }
             System.out.println();
         }
