7ab9000ea403e453c555a2c1e5fbfb8fd00c8de3
Srinivas Manikanta <97425800+S-1729@users.noreply.github.com>
2023-01-31 23:10:11 +0530

    import java.util.Scanner; class solution{     public static void main(String args[]){         Scanner scan=new Scanner(System.in);         String str=scan.nextLine();         String st=str.toLowerCase();         System.out.println(st);
        }
    }

---
+++ To_Lower_Case.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class solution{
+    public static void main(String args[]){
+        Scanner scan=new Scanner(System.in);
+        String str=scan.nextLine();
+        String st=str.toLowerCase();
+        System.out.println(st);
+        
+    }
+}
